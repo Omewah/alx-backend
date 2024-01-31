04106e0f57e3c6f1eb034485fa2892bf1499f4fa
Omewah <joelomewah@ymail.com>
2024-01-31 18:49:25 +0100

    add readme

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic dictionary"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """BasicCache"""
+    def put(self, key, item):
+        """assigns the new item to dict"""
+        if not (key is None or item is None):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """returns the value in self.cache_data"""
+        if key is None or not (key in self.cache_data):
+            return None
+        return self.cache_data[key]
